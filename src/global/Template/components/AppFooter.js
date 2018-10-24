1fda7965fb7428f41b60631fac4025b76583201d
ronen.goren <ronen.goren@gmail.com>
2018-10-24 00:17:02 -0400

    running

--- src/global/Template/components/AppFooter.js
+++ src/global/Template/components/AppFooter.js
@@ -9,85 +9,85 @@ import { isFareStructureEquals } from "../../../global"
 import { taxiTypes } from "../../../global"
 
 const styles = {
-  footerContainer: {
-    backgroundColor: "#fff"
-  },
-  type: {
-    fontSize: 12
-  },
-  title: {
-    fontSize: 6
-  }
+    footerContainer: {
+        backgroundColor: "#fff"
+    },
+    type: {
+        fontSize: 12
+    },
+    title: {
+        fontSize: 6
+    }
 }
 
 export default class AppFooter extends Component {
-  onTaxiTypeSelected = taxiType => {
-    if (taxiType.type !== this.props.selectedTaxiType.type) {
-      this.props.setSelectedTaxiType(taxiType)
+    onTaxiTypeSelected = taxiType => {
+        if (taxiType.type !== this.props.selectedTaxiType.type) {
+            this.props.setSelectedTaxiType(taxiType)
+        }
     }
-  }
 
-  componentDidMount() {
-    this.props.setSelectedTaxiType(taxiTypes[0])
-  }
-
-  formatTabElement(tabInfo, style = {}) {
-    // Clone the styles so that customization will not impacted to template
-    const elementStyle = { ...style }
-    const tabIsActive = this.props.selectedTaxiType === tabInfo
-    elementStyle.color = tabIsActive ? "#FF5E3A" : "grey"
-
-    return elementStyle
-  }
-
-  renderTab(tabInfo) {
-    // FIXME using anonymous function is not the good practices,
-    // are there anyway to pass arguments via onPress event?
-    const subTitle = tabInfo.pricing ? `$${tabInfo.pricing}` : tabInfo.title
-    return (
-      <Button vertical key={tabInfo.type} onPress={() => this.onTaxiTypeSelected(tabInfo)}>
-        <Icon size={25} name={tabInfo.icon} style={this.formatTabElement(tabInfo)} />
-        <Text style={this.formatTabElement(tabInfo, styles.type)}>{tabInfo.type}</Text>
-        <Text style={this.formatTabElement(tabInfo, styles.title)}>{subTitle}</Text>
-      </Button>
-    )
-  }
-
-  render() {
-    return (
-      <Footer>
-        <FooterTab style={styles.footerContainer}>
-          {taxiTypes.map(type => this.renderTab(type))}
-        </FooterTab>
-      </Footer>
-    )
-  }
-
-  componentWillUpdate = (nextProps, nextState) => {
-    // Recalculate price when new fare structure is applied.
-    const newFare = nextProps.fareStructure
-    if (!isFareStructureEquals(this.props.fareStructure, newFare)) {
-      taxiTypes.forEach(type => {
-        // Travel < 5km will be priced as 5 km.
-        const distance = Math.max(3, newFare.distance.value / 1000)
-        const standardDuration = distance * type.standardDurationPerKm
-        const durationRate = newFare.duration.value / standardDuration
-        const price = durationRate * type.pricePerKm * distance
-        type.pricing = Math.round(price * 100) / 100
-      })
+    componentDidMount() {
+        this.props.setSelectedTaxiType(taxiTypes[0])
     }
-  }
+
+    formatTabElement(tabInfo, style = {}) {
+        // Clone the styles so that customization will not impacted to template
+        const elementStyle = { ...style }
+        const tabIsActive = this.props.selectedTaxiType === tabInfo
+        elementStyle.color = tabIsActive ? "#FF5E3A" : "grey"
+
+        return elementStyle
+    }
+
+    renderTab(tabInfo) {
+        // FIXME using anonymous function is not the good practices,
+        // are there anyway to pass arguments via onPress event?
+        const subTitle = tabInfo.pricing ? `$${tabInfo.pricing}` : tabInfo.title
+        return (
+            <Button vertical key={tabInfo.type} onPress={() => this.onTaxiTypeSelected(tabInfo)}>
+                <Icon size={25} name={tabInfo.icon} style={this.formatTabElement(tabInfo)} />
+                <Text style={this.formatTabElement(tabInfo, styles.type)}>{tabInfo.type}</Text>
+                <Text style={this.formatTabElement(tabInfo, styles.title)}>{subTitle}</Text>
+            </Button>
+        )
+    }
+
+    render() {
+        return (
+            <Footer>
+                <FooterTab style={styles.footerContainer}>
+                    {taxiTypes.map(type => this.renderTab(type))}
+                </FooterTab>
+            </Footer>
+        )
+    }
+
+    componentWillUpdate = (nextProps, nextState) => {
+        // Recalculate price when new fare structure is applied.
+        const newFare = nextProps.fareStructure
+        if (!isFareStructureEquals(this.props.fareStructure, newFare)) {
+            taxiTypes.forEach(type => {
+                // Travel < 5km will be priced as 5 km.
+                const distance = Math.max(3, newFare.distance.value / 1000)
+                const standardDuration = distance * type.standardDurationPerKm
+                const durationRate = newFare.duration.value / standardDuration
+                const price = durationRate * type.pricePerKm * distance
+                type.pricing = Math.round(price * 100) / 100
+            })
+        }
+    }
 }
 
 AppFooter.propTypes = {
-  setSelectedTaxiType: PropTypes.func.isRequired,
-  selectedTaxiType: PropTypes.object,
-  fareStructure: PropTypes.shape({
-    distance: PropTypes.shape({
-      value: PropTypes.number.isRequired
-    }).isRequired,
-    duration: PropTypes.shape({
-      value: PropTypes.number.isRequired
-    }).isRequired
-  })
+    setSelectedTaxiType: PropTypes.func.isRequired,
+    selectedTaxiType: PropTypes.object,
+    fareStructure: PropTypes.shape({
+        distance: PropTypes.shape({
+            value: PropTypes.number.isRequired
+        }).isRequired,
+        duration: PropTypes.shape({
+            value: PropTypes.number.isRequired
+        }).isRequired
+    })
 }
--- src/routes/Home/components/SearchBox/index.js
+++ src/routes/Home/components/SearchBox/index.js
@@ -10,84 +10,84 @@ import RNGooglePlaces from "react-native-google-places"
 import { showError } from "../../../../global"
 
 export class SearchBox extends Component {
-  choosePickupLocation = () => {
-    const latLngBounds = {}
-    const { pickupLocation } = this.props
-    if (pickupLocation) {
-      latLngBounds.latitude = pickupLocation.latitude
-      latLngBounds.longitude = pickupLocation.longitude
+    choosePickupLocation = () => {
+        const latLngBounds = {}
+        const { pickupLocation } = this.props
+        if (pickupLocation) {
+            latLngBounds.latitude = pickupLocation.latitude
+            latLngBounds.longitude = pickupLocation.longitude
+        }
+        this.pickALocation(latLngBounds, this.props.setPickupLocation)
     }
-    this.pickALocation(latLngBounds, this.props.setPickupLocation)
-  }
 
-  chooseDropoffLocation = () => {
-    const latLngBounds = {}
-    const { dropoffLocation } = this.props
-    if (dropoffLocation) {
-      latLngBounds.latitude = dropoffLocation.latitude
-      latLngBounds.longitude = dropoffLocation.longitude
+    chooseDropoffLocation = () => {
+        const latLngBounds = {}
+        const { dropoffLocation } = this.props
+        if (dropoffLocation) {
+            latLngBounds.latitude = dropoffLocation.latitude
+            latLngBounds.longitude = dropoffLocation.longitude
+        }
+        this.pickALocation(latLngBounds, this.props.setDropLocation)
     }
-    this.pickALocation(latLngBounds, this.props.setDropLocation)
-  }
 
-  pickALocation(latLngBounds = {}, successCallback) {
-    latLngBounds.radius = 0.1
-    RNGooglePlaces.openPlacePickerModal(latLngBounds)
-      .then(place => successCallback(place))
-      .catch(error => showError(error.message))
-  }
+    pickALocation(latLngBounds = {}, successCallback) {
+        latLngBounds.radius = 0.1
+        RNGooglePlaces.openPlacePickerModal(latLngBounds)
+            .then(place => successCallback(place))
+            .catch(error => showError(error.message))
+    }
 
-  getLocationDisplayText = location => {
-    return location
-      ? location.name || location.address || `${location.latitude},${location.longitude}`
-      : ""
-  }
+    getLocationDisplayText = location => {
+        return location
+            ? location.name || location.address || `${location.latitude},${location.longitude}`
+            : ""
+    }
 
-  render() {
-    const dropoffText = this.getLocationDisplayText(this.props.dropoffLocation)
-    const pickupText = this.getLocationDisplayText(this.props.pickupLocation)
-    return (
-      <View style={styles.searchBox}>
-        <View style={styles.inputWrapper}>
-          <InputGroup>
-            <Icon name="search" size={15} color="green" />
-            <Input
-              style={styles.inputSearch}
-              placeholder="Choose pick-up location"
-              value={pickupText}
-              onFocus={this.choosePickupLocation}
-            />
-          </InputGroup>
-          <InputGroup>
-            <Icon name="search" size={15} color="orangered" />
-            <Input
-              style={styles.inputSearch}
-              placeholder="Choose drop-off location"
-              value={dropoffText}
-              onFocus={this.chooseDropoffLocation}
-            />
-          </InputGroup>
-        </View>
-      </View>
-    )
-  }
+    render() {
+        const dropoffText = this.getLocationDisplayText(this.props.dropoffLocation)
+        const pickupText = this.getLocationDisplayText(this.props.pickupLocation)
+        return (
+            <View style={styles.searchBox}>
+                <View style={styles.inputWrapper}>
+                    <InputGroup>
+                        <Icon name="search" size={15} color="green" />
+                        <Input
+                            style={styles.inputSearch}
+                            placeholder="Choose pick-up location"
+                            value={pickupText}
+                            onFocus={this.choosePickupLocation}
+                        />
+                    </InputGroup>
+                    <InputGroup>
+                        <Icon name="search" size={15} color="orangered" />
+                        <Input
+                            style={styles.inputSearch}
+                            placeholder="Choose drop-off location"
+                            value={dropoffText}
+                            onFocus={this.chooseDropoffLocation}
+                        />
+                    </InputGroup>
+                </View>
+            </View>
+        )
+    }
 }
 
 SearchBox.propTypes = {
-  setPickupLocation: PropTypes.func.isRequired,
-  pickupLocation: PropTypes.shape({
-    name: PropTypes.string,
-    address: PropTypes.string,
-    latitude: PropTypes.number.isRequired,
-    longitude: PropTypes.number.isRequired
-  }),
-  setDropLocation: PropTypes.func.isRequired,
-  dropoffLocation: PropTypes.shape({
-    name: PropTypes.string,
-    address: PropTypes.string,
-    latitude: PropTypes.number.isRequired,
-    longitude: PropTypes.number.isRequired
-  })
+    setPickupLocation: PropTypes.func.isRequired,
+    pickupLocation: PropTypes.shape({
+        name: PropTypes.string,
+        address: PropTypes.string,
+        latitude: PropTypes.number.isRequired,
+        longitude: PropTypes.number.isRequired
+    }),
+    setDropLocation: PropTypes.func.isRequired,
+    dropoffLocation: PropTypes.shape({
+        name: PropTypes.string,
+        address: PropTypes.string,
+        latitude: PropTypes.number.isRequired,
+        longitude: PropTypes.number.isRequired
+    })
 }
 
 export default SearchBox
